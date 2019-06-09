b705db5f8e9869fc58928543a580805af15b3d96
cffirm <cffirm0918@163.com>
2019-06-09 15:37:45 +0800

    shop_lrg_06/09

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -7,4 +7,10 @@ use Illuminate\Http\Request;
 class MyController extends Controller
 {
     //
+    public function index(){
+    	return view('admin/index');
+    }
+    public function index_v1(){
+    	return view('admin/index_v1');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Mycontroller@index');
+Route::get('/index_v1', 'Mycontroller@index_v1');
